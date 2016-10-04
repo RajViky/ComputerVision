fce8609bcf676d6d598046298e8a821c26f97b76
Tomas <tomas.bedrnik@email.czcd>
2016-10-04 13:35:13 +0300

    Still now working

--- PCL-RealSense-Test/PCL-RealSense-Test.pro
+++ PCL-RealSense-Test/PCL-RealSense-Test.pro
@@ -4,3 +4,14 @@ CONFIG -= app_bundle
 CONFIG -= qt
 
 SOURCES += main.cpp
+
+INCLUDEPATH += /home/beda/data/skola/_Oulu/PCL-libRealSense/pkg/pcl-librealsense-git/usr/include/pcl-1.8 \
+    /usr/include/eigen3/ \
+    /usr/include/vtk/
+
+QMAKE_FLAGS += "-rpath=/home/beda/data/skola/_Oulu/PCL-libRealSense/pkg/pcl-librealsense-git/usr/lib/"
+
+LIBS += -lboost_system \
+    -lboost_thread \
+    /home/beda/data/skola/_Oulu/PCL-libRealSense/pkg/pcl-librealsense-git/usr/lib/libpcl_io.so.1.8.0 \
+    /home/beda/data/skola/_Oulu/PCL-libRealSense/pkg/pcl-librealsense-git/usr/lib/libpcl_visualization.so.1.8.0
--- PCL-RealSense-Test/main.cpp
+++ PCL-RealSense-Test/main.cpp
@@ -1,9 +1,42 @@
-#include <iostream>
+#include <pcl/io/real_sense_grabber.h>
+#include <pcl/visualization/cloud_viewer.h>
 
-using namespace std;
-
-int main(int argc, char *argv[])
+class SimpleRealSenseViewer
 {
-    cout << "Hello World!" << endl;
-    return 0;
+  public:
+    SimpleRealSenseViewer () : viewer ("PCL OpenNI Viewer") {}
+
+    void cloud_cb_ (const pcl::PointCloud<pcl::PointXYZ>::ConstPtr &cloud)
+    {
+      if (!viewer.wasStopped())
+        viewer.showCloud (cloud);
+    }
+
+    void run ()
+    {
+      pcl::Grabber* interface = new pcl::RealSenseGrabber();
+
+      boost::function<void (const pcl::PointCloud<pcl::PointXYZ>::ConstPtr&)> f =
+        boost::bind (&SimpleRealSenseViewer::cloud_cb_, this, _1);
+
+      interface->registerCallback (f);
+
+      interface->start ();
+
+      while (!viewer.wasStopped())
+      {
+        boost::this_thread::sleep (boost::posix_time::seconds (1));
+      }
+
+      interface->stop ();
+    }
+
+    pcl::visualization::CloudViewer viewer;
+};
+
+int main ()
+{
+  SimpleRealSenseViewer v;
+  v.run ();
+  return 0;
 }
